e21eb6288738cd10d3e5040b39bd556f16178720
Jahid Rahman <rahmanjahid645@gmail.com>
2026-05-03 23:47:46 +0600

    ES6

---
+++ ES6/main.js
@@ -0,0 +1,35 @@
+console.log('Hello, ES6!');
+
+// Basic Syntax
+
+// 3 Basic Syntax
+
+// Example of let and const
+// let name = 'Alice';
+// const age = 30;
+// console.log(`Name: ${name}, Age: ${age}`);
+
+// // Example of arrow functions
+// const add = (a, b) => a + b;
+// console.log(`Sum: ${add(5, 3)}`);
+
+// // Example of template literalsconst greeting = `Hello, ${name}! You are ${age} years old.`;
+// console.log(greeting);  
+
+// // Example of destructuringconst person = { firstName: 'Bob', lastName: 'Smith' };
+// const { firstName, lastName } = person;
+// console.log(`First Name: ${firstName}, Last Name: ${lastName}`);
+
+// // Example of classes
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     greet() {       return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+//     }
+// }
+
+// const person1 = new Person('Charlie', 25);
+// console.log(person1.greet());
