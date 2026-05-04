3a6881afc017cd6136069376780c3970a2cb59bc
Jahid Rahman <rahmanjahid645@gmail.com>
2026-05-04 13:23:48 +0600

    ES6(basic syntax, Strict Mode, spread Operator with & without, parameter, function)

--- ES6/main.js
+++ ES6/main.js
@@ -1,8 +1,73 @@
-console.log('Hello, ES6!');
+// console.log('Hello, ES6!');
 
 // Basic Syntax
 
-// 3 Basic Syntax
+// Strict Mode
+// 'use strict';
+var name = "Jahid Rahman";
+MyName();
+function MyName() {
+    // name = "Jahid Rahman";
+    // console.log(name);
+}
+
+// 5 Spread Operator
+// let poorCountry = ['Bangladesh', 'Nepal', 'Bhutan'];
+// let richCountry = ['USA', 'UK', 'Germany'];
+// let allCountry = [...poorCountry, ...richCountry];
+
+// another wahy to write
+// let allCountry = poorCountry.concat(['USA', 'UK', 'Germany']);
+
+// another way to write
+// let allCountry = [...poorCountry, 'USA', 'UK', 'Germany'];
+// console.log(allCountry);
+
+// 6 Without Spread Operator
+// let poorCountry = ['Bangladesh', 'Nepal', 'Bhutan'];
+// let richCountry = ['USA', 'UK', 'Germany'];
+// richCountry.push(...poorCountry); //output will be ['USA', 'UK', 'Germany', 'Bangladesh', 'Nepal', 'Bhutan']
+// richCountry.push(poorCountry); // output will be ['USA', 'UK', 'Germany', ['Bangladesh', 'Nepal', 'Bhutan']]
+// console.log(richCountry);
+
+// 7 Rest Parameter
+// function CaulculateSum(a,b,...numbers) {
+//     let sum = 0;
+//     for(let i of numbers) {
+//         sum += i;
+//     }
+//     console.log(`Sum: ${sum}`);
+// }
+// CaulculateSum(1, 2, 3, 4, 5); // output will be Sum: 15
+
+// 8 Dynamic Function
+// var name=function(nameValue){
+//     return nameValue;
+// }
+// console.log(name('Jahid Rahman'));
+
+// 9 ES6 Variable [var dia re-declare and re-assign kora jay, let dia re-declare kora jay na but re-assign kora jay, const dia re-declare o re-assign kora jay na]
+
+// var name="jahid rahman";;
+// name = "rainbow"; // re-assign kora jay
+// console.log(name);
+
+// const name="jahid rahman";;
+// name = "rainbow"; // re-assign kora jay na, error dekhabe
+// console.log(name);
+
+// var name = "Jahid Rahman";
+// var name = "Rainbow"; // re-declare kora jay, error dekhabe na
+// console.log(name);
+
+// let name="jahid rahman";;
+// let name = "rainbow"; // re-declare kora jay na, error dekhabe
+// console.log(name);
+
+const name="jahid rahman";;
+const name = "rainbow"; // re-declare kora jay na, error dekhabe
+console.log(name);
+
 
 // Example of let and const
 // let name = 'Alice';
